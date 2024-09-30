af188ccef245331d4cf0ee1c7bc2b6eb9c6cbf8c
Lucas Drieberg <lucasdrie@outlook.com>
2024-09-30 18:51:00 +1000

    update menu

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -3,11 +3,14 @@ import { header } from "framer-motion/client";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { MENU_ITEMS } from "../_constants";
+import { usePathname } from "next/navigation";
 // import { Menu } from "./Menu/Menu";
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
+    const currentPath = usePathname();
 
     const handleMenuButtonClick = function () {
         setIsMenuOpen(prevState => !prevState);
@@ -19,7 +22,6 @@ export default function Header() {
                 setIsMenuOpen(false);
             }
         }
-
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
@@ -41,37 +43,20 @@ export default function Header() {
                     </button>
                 </div >
                 <div className={`rounded bg-blue-500 w-[10rem] z-10 ${isMenuOpen ? "block" : "hidden"}`}>
-                    <ul className=" flex flex-col gap-1 select-text text-white">
-                        <li className="w-full">
-                            <Link href="/" className="flex items-center justify-between py-4 pl-1 pr-4 hover:bg-blue-600"
-                                onClick={() => setIsMenuOpen(0)}>
-                                <Image className="w-8 h-8" src={`/icons/home_24dp_FILL0_wght400_GRAD0_opsz24.svg`} alt="navigate to home us page" height={10} width={10} />
-                                Home
-                            </Link>
-                        </li>
-                        <li className="">
-                            <Link
-                                onClick={() => setIsMenuOpen(0)}
-                                href="/about" className="flex items-center justify-between py-4 pl-1 pr-4 hover:bg-blue-600">
-                                <Image className="w-8 h-8" src={`/icons/person_24dp_FILL0_wght400_GRAD0_opsz24.svg`} alt="navigate to about us page" height={10} width={10} />
-                                About
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                onClick={() => setIsMenuOpen(0)}
-                                href="/works/" className="flex items-center justify-between py-4 pl-1 pr-4 hover:bg-blue-600">
-                                <Image className="w-8 h-8" src={`/icons/article_24dp_FILL0_wght400_GRAD0_opsz24.svg`} alt="navigate to about liliana's creative works page" height={10} width={10} />
-                                Works
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                onClick={() => setIsMenuOpen(0)}
-                                href="/contact-us" className="flex items-center justify-between py-4 pl-1 pr-4 hover:bg-blue-600 ">
-                                <Image className="w-8 h-8" src={`/icons/mail_24dp_FILL0_wght400_GRAD0_opsz24.svg`} alt="navigate to about liliana's contact us page" height={10} width={10} /> Contact
-                            </Link>
-                        </li>
+                    <ul className=" flex flex-col  select-text text-white">
+                        {MENU_ITEMS.map((item) => (
+                            <li key={item.href} className="w-full">
+                                <Link
+                                    href={item.href}
+                                    className={`flex items-center justify-between py-4 pl-1 pr-4 ${currentPath === item.href ? 'bg-blue-600' : 'hover:bg-blue-600'
+                                        }`}
+                                    onClick={() => setIsMenuOpen(false)}
+                                >
+                                    <Image className="w-8 h-8" src={item.icon} alt={`Navigate to ${item.label} page`} height={10} width={10} />
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
--- src/app/_constants/index.ts
+++ src/app/_constants/index.ts
@@ -1,7 +1,9 @@
 import { BASE_URL } from "./BaseUrl/BaseUrl";
+import { MENU_ITEMS } from "./Menu/MenuItems";
 import { WORKS_DATA } from "./WorksData/WorksData";
 
 export {
 BASE_URL,
+MENU_ITEMS,
 WORKS_DATA
 }
